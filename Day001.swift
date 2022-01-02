42e471e8074e0255d14b2a1a3e96a021bfbe137c
Shimoorook <jyoon@esri.com>
2022-01-01 21:21:53 -0800

    One page.

---
+++ Day001.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  TheBeginning
+//
+//  Created by to-apple on 1/1/22.
+//
+
+import Foundation
+
+// Variables are always initialized before use. -> What does this mean?
+// Array indices are checked for out-of-bounds errors -> Doesn't this require additional computation?
+// Integers are checked for overflow. -> Again, what does this mean?
+// Optionals ensure that nil values are handled explicitly -> Again, computation
+// Memory is managed automatically -> What does this mean?
+// Error handling allows controlled recovery from unexpected failures.
+
+// 'The obvious way to write your code should also perform the best' Cool
