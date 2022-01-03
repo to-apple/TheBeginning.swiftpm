cc7b96aba2e7618a0719b8ed1d98962a3643d99b
Shimoorook <jyoon@esri.com>
2022-01-02 20:39:03 -0800

    Simple Types.

--- Day001-002.swift
+++ Day001-002.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+// Day.001
+
 // Variables are always initialized before use. -> What does this mean?
 // Array indices are checked for out-of-bounds errors -> Doesn't this require additional computation?
 // Integers are checked for overflow. -> Again, what does this mean?
@@ -15,3 +17,19 @@ import Foundation
 // Error handling allows controlled recovery from unexpected failures.
 
 // 'The obvious way to write your code should also perform the best' Cool
+
+// Day.002
+// Simple values
+// Can be used with 'var' or 'let'
+// Type can be inferred, but not implicitly converted to another type
+// String interpolation is used with backslash '\(variable)'
+// Three double quotation marks for multiline string (""" MULTILINE STRING"""
+// Brackets to create both array and dictionaries ([])
+// Empty array and dictionaries can be created with initializers
+// eg. let emptyArray: [String] = []
+// eg2. let emptyDictionary: [String: Float] = []
+// if the type can be inferred, the code below can be used
+// eg. shoppingList = []
+// eg2. occupations = [:]
+
+
