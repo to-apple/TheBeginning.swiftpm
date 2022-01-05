4a60dd074c0838589b2face01bd0293ca2ddd148
to-apple <96967974+to-apple@users.noreply.github.com>
2022-01-04 20:56:37 -0800

    Update and rename Day001-002.swift to Day001-004.swift

--- Day001-004.swift
+++ Day001-004.swift
@@ -48,3 +48,57 @@ import Foundation
 //    print("Everything tastes good in soup.")
 //}
 // Ranges can be ..< or ...
+
+// Day.004
+// Functions and Closures
+// Tuples can be used as a return value. Use index or variable name to access it
+
+//func calcuateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+//    var min = scores[0]
+//    var max = scores[0]
+//    var sum = 0
+//
+//    for score in scores {
+//        if score > max {
+//            max = score
+//        } else if score < min {
+//            min = score
+//        }
+//        sum += score
+//    }
+//    return (min, max, sum)
+//}
+//
+//let statistics = calcuateStatistics(scores: [5, 3, 100, 3, 9])
+//print(statistics.sum)
+//print(statistics.2)
+
+// Functions are first-class types and can be used just like a regular variable
+//
+//func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
+//    for item in list {
+//        if condition(item) {
+//            return true
+//        }
+//    }
+//    return false
+//}
+//
+//func lessThanTen(number: Int) -> Bool {
+//    return number < 10
+//}
+//
+//var numbers = [20, 19 ,7, 12]
+//
+//hasAnyMatches(list: numbers, condition: lessThanTen)
+
+// Functions are actually a special case of closures: blocks of code that can be called later.
+//
+//numbers.map({ (number: Int) -> Int in
+//    let result = 3 * number
+//    return result
+//})
+//
+//let mappedNumbers = number.map({ number in 3 * number })
+//
+//let sortedNumbers = numbers.sorted { $0 > $1 }
