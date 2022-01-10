e8c4553cfe64e24a735226c43ae896fb54906c7c
to-apple <96967974+to-apple@users.noreply.github.com>
2022-01-09 21:15:19 -0800

    Update and rename Day001-008.swift to Day001-009.swift

--- Day001-009.swift
+++ Day001-009.swift
@@ -116,7 +116,52 @@ import Foundation
 // Fighting the Booster shot.
 
 // Day.007
-// Tired. 
+// Tired.
 
 // Day.008
 // Recovery last day.
+
+// Day.009
+// Enumerations and Structures
+// By default raw values start at zero
+// init?(rawValue:) created an enumeration for a raw value
+// This is the opposite or creating an enum case and checking the raw value I guess
+
+//enum Rank: Int {
+//    case ace = 1
+//    case two, three, four, five, six, seven, eight, nine, ten
+//    case jack, queen, king
+//
+//    func simpleDescription() -> String {
+//        switch self {
+//        case .ace:
+//            return "ace"
+//        case .jack:
+//            return "jack"
+//        case .queen:
+//            return "queen"
+//        case .king:
+//            return "king"
+//        default:
+//            return String(self.rawValue)
+//        }
+//    }
+//}
+//
+//let ace = Rank.ace
+//let aceRawValue = ace.rawValue
+
+//enum ServerResponse {
+//    case result(String, String)
+//    case failure(String)
+//}
+//
+//let success = ServerResponse.result("6:00 am", "8:09 pm")
+//let failure = ServerResponse.failure("Out of cheese")
+//
+//switch success {
+//case let .result(sunrise, sunset):
+//    print("Sunrise is at \(sunrise) and sunset is at \(sunset)")
+//case let .failure(message):
+//    print("Failure... \(message)")
+//}
