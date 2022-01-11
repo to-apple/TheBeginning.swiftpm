b50662e91d7109f605196bf313fd52f0886b43bb
to-apple <96967974+to-apple@users.noreply.github.com>
2022-01-10 21:11:50 -0800

    Update and rename Day001-009.swift to Day001-010.swift

--- Day001-010.swift
+++ Day001-010.swift
@@ -165,3 +165,36 @@ import Foundation
 //case let .failure(message):
 //    print("Failure... \(message)")
 //}
+
+// Day.010
+// Protocols and Extensions
+
+//protocol ExampleProtocol {
+//    var simpleDescription: String { get }
+//    mutating func adjust()
+//}
+//
+//class SimpleClass: ExampleProtocol {
+//    var simpleDescription: String = "A very simple class."
+//    var anotherProperty: Int = 69105
+//    func adjust() {
+//        simpleDescription += " Now 100% adjusted"
+//    }
+//}
+//
+//struct SimpleStructure: ExampleProtocol {
+//    var simpleDescription: String = "A simple structure"
+//    mutating func adjust() {
+//        simpleDescription += " (adjusted)"
+//    }
+//}
+//
+//extension Int: ExampleProtocol {
+//    var simpleDescription: String {
+//        return "The number \(self)"
+//    }
+//    
+//    mutating func adjust() {
+//        self += 42
+//    }
+//}
